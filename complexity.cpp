846929cf0b7d3b2ddc568267f5da4f63eb2ceb60
Simon Cros <scros@student.42lyon.fr>
2021-10-26 17:15:08 +0200

    First version of complexity in c++

--- complexity.cpp
+++ complexity.cpp
@@ -2,6 +2,7 @@
 #include <getopt.h>
 #include <algorithm>
 #include <vector>
+#include <array>
 #include <random>
 #include <string>
 #include <iterator>
@@ -9,7 +10,6 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <optional>
-#include <boost
 
 using namespace std;
 
@@ -32,9 +32,9 @@ static const string getHelp()
 {
 	string help;
 
-	help += "\033[1;31mUsage :\n";
-	help += "\033[1;30m./complexity [-h|--help]\033[0m\n";
-	help += "\033[1;30m./complexity [-s|--sorted] <nb_item> <nb_iter> [goal] [checker]\033[0m";
+	help += "\033[91mUsage :\n";
+	help += "\033[90m./complexity [-h|--help]\033[0m\n";
+	help += "\033[90m./complexity [-s|--sorted] <nb_item> <nb_iter> [goal] [checker]\033[0m";
 	return help;
 }
 
@@ -78,7 +78,7 @@ static struct program_opts getOptions(int& argc, char **&argv)
 		{NULL, 0, NULL, 0}
 	};
 
-	program_opts opts;
+	program_opts opts = {false};
 
 	int ch;
 	while ((ch = getopt_long(argc, argv, "hs", long_options, NULL)) != -1)
@@ -125,6 +125,35 @@ static void showCursor()
 	cout << "\e[?25h";
 }
 
+static std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
+static void print(program_params params, int done, int total, int best, int worst, int successful, int ok)
+{
+	cout << "Pire = \033[31m" << (worst) << "\033[0m instructions" << endl;
+	cout << "Moyenne = \033[33m" << (total / done) << "\033[0m instructions" << endl;
+	cout << "Meilleur = \033[36m" << (best) << "\033[0m instructions" << endl;
+	if (params.objective.has_value())
+		cout << "Objectif = \033[94m" << (successful * 100 / done) << "\033[0m % sous \033[94m" << (params.objective.value()) << "\033[0m (\033[91m" << (done - successful) << "\033[0m au dessus)   " << endl;
+	else
+		cout << "Objectif = entrez un nombre en troisième argument" << endl;
+	if (params.checker.has_value())
+		cout << "Précision = \033[97m" << (ok * 100 / done) << "\033[0m % OK (\033[91m" << (done - ok) << "\033[0m KO)   " << endl;
+	else
+		cout << "Précision = entrez un testeur en quatrième argument" << endl;
+	cout << "\033[32m" << (done * 100 / params.iterations) << "\033[0m % effectué" << endl;
+}
+
 int main(int argc, char **argv)
 {
 	program_opts opts;
@@ -147,36 +176,57 @@ int main(int argc, char **argv)
 		return EXIT_FAILURE;
 	}
 
-	signal(SIGINT, (void (*)(int))showCursor);
+	atexit([]() {
+		showCursor();
+		cout << "\033[6B\033[0m";
+	});
+	signal(SIGINT, [](int s) {
+		exit(EXIT_SUCCESS);
+	});
 	
 	auto rd = std::random_device {}; 
 	auto rng = std::default_random_engine { rd() };
 
-	for (int i = 0; i < params.iterations; i++)
+	int done = 0;
+	int worst = 0;
+	int best = -1;
+	int	total = 0;
+	int successful = 0;
+	int ok = 0;
+
+	hideCursor();
+	cout << "\033[97mDémarrage du test : \033[95m" << params.numbers << "\033[97m éléments, \033[95m" << params.iterations << "\033[97m itérations" << endl;
+	while (done < params.iterations)
 	{
 		vector<int> nums(params.numbers);
 		vector<string> args;
-		vector<char*> cargs;
 
 		iota(begin(nums), end(nums), 0);
-		std::shuffle(nums.begin(), nums.end(), rng);
+		if (!opts.sorted)
+			std::shuffle(nums.begin(), nums.end(), rng);
 		std::transform(nums.begin(), nums.end(), std::back_inserter(args), [](int d) {
 				return std::to_string(d);
 			}
 		);
+		std::string command = params.program;
+		for (const string &piece : args)
+			command += " " + piece;
 
-        for(string& arg : args)
-            cargs.push_back(&arg.front());
+        string result = exec(command.c_str());
+		int lines = std::count(result.begin(), result.end(), '\n');
 
-		
+		done++;
+		total += lines;
 
+		if (lines <= params.objective.value_or(-1))
+			successful++;
+		if (lines < best || best == -1)
+			best = lines;
+		if (lines > worst)
+			worst = lines;
 
-		// copy(args.begin(), args.end(), ostream_iterator<string>(cout, "\n"));
+		print(params, done, total, best, worst, successful, 0);
+		cout << "\033[6A";
 	}
-
-	// hideCursor();
-	// sleep(2);
-	// showCursor();
-
 	return EXIT_SUCCESS;
 }
